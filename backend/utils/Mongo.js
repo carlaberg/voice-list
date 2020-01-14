2ab8a74cbe93845ca2d6f456c0e7f41459ddbec9
carlaberg <mail.carl.aberg@gmail.com>
2020-01-14 21:53:23 +0100

    Changed .gitignore

--- backend/utils/Mongo.js
+++ backend/utils/Mongo.js
@@ -7,18 +7,20 @@ const MongoConnector = () => {
     function setConnectionString() {
         const {
             NODE_ENV,
-            MONGO_USERNAME,
-            MONGO_PASSWORD,
             MONGO_HOSTNAME,
             MONGO_PORT,
-            MONGO_DB
+            MONGO_USER_PROD,
+            MONGO_PASSWORD_PROD,
+            MONGO_HOSTNAME_PROD,
+            MONGO_PORT_PROD,
+            MONGO_DB_PROD
         } = process.env;
         console.log('hostname: ', MONGO_HOSTNAME);
         // Connect to mongodb without authentication in development
         if ( NODE_ENV == 'development') {
             return `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
         } else {
-            return `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+            return `mongodb://${MONGO_USER_PROD}:${MONGO_PASSWORD_PROD}@${MONGO_HOSTNAME_PROD}:${MONGO_PORT_PROD}/${MONGO_DB_PROD}`;
         }
     }
 
