f9c3e16960a1607e84d27a6aeb4799c79d9bdc27
carlaberg <mail.carl.aberg@gmail.com>
2020-02-03 20:30:38 +0100

    Changed connection to mongo

--- functions/graphql-api/utils/Mongo.js
+++ functions/graphql-api/utils/Mongo.js
@@ -3,30 +3,13 @@ const mongoose = require('mongoose');
 const MongoConnector = () => {
     
     let dbInstance = {};
-    
-    function setConnectionString() {
-        const {
-            NODE_ENV,
-            MONGO_HOSTNAME,
-            MONGO_PORT,
-            MONGO_DB,
-            MONGO_USER,
-            MONGO_PASSWORD
-        } = process.env;
-
-        // Connect to mongodb without authentication in development
-        if ( NODE_ENV == 'development') {
-            return `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
-        } else {
-            return `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
-        }
-    }
 
     async function initDb() {
-        const url = setConnectionString();
-        
         try {
-            dbInstance = await mongoose.connect(url, { useNewUrlParser: true });
+            dbInstance = await mongoose.connect(process.env.MONGO_CONNECTION_STR, { 
+                useNewUrlParser: true,
+                useUnifiedTopology: true 
+            });
             console.log('successfully connected to mongodb');
         } catch (error) {
             console.error(error);
