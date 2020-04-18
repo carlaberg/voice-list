422095dbed55ead10b1c715ad61b91b1f3c85901
carlaberg <mail.carl.aberg@gmail.com>
2020-04-18 21:54:23 +0200

    Added more logs

--- functions/graphql-api/utils/Mongo.js
+++ functions/graphql-api/utils/Mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const MongoConnector = () => {
-
+    console.log('connection string: ', process.env.MONGO_CONNECTION_STR)
     async function initDb() {
         try {
             const db = await mongoose.connect(process.env.MONGO_CONNECTION_STR, { 
